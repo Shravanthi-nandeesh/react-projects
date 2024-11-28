f64e02764473fdf5c1e2db8f0ab442a4273102e9
shravanthi nandeesh <106576060+Shravanthi-nandeesh@users.noreply.github.com>
2024-11-28 15:27:01 +0530

    second comit

---
+++ app/index.js
@@ -0,0 +1,58 @@
+import { View, Text, StyleSheet, SafeAreaView, ScrollView, StatusBar } from "react-native";
+import { Stack, useRouter } from "expo-router";
+import { useState } from "react";
+import { COLORS, SIZES, FONT } from "../constants";
+import NearByJobs from "../components/NearBy/NearBy";
+import Welcome from "../components/Welcome/Welcome";
+import PopularJobs from "../components/PopularJobs/PopularJobs";
+import HeaderLeftBtn from "../components/common/HeaderLeftBtn";
+import HeaderRightBtn from "../components/common/HeaderRightBtn";
+const Home = () => {
+    const router = useRouter();
+
+    return (
+        <SafeAreaView style={styles.SafeAreaView}>
+            <StatusBar
+                barStyle={'light-content'}
+                backgroundColor={COLORS.primary}
+            />
+            <Stack.Screen
+                options={{
+                    headerStyle: { backgroundColor: COLORS.background },
+                    headerShadowVisible: false,
+                    headerLeft: () => (
+                        <HeaderLeftBtn />
+                    ),
+                    headerRight: () => (
+                        <HeaderRightBtn />
+                    ),
+                    headerTitle: ""
+                }}
+            />
+            <ScrollView showsVerticalScrollIndicator={false}>
+                <View style={styles.HomePage}>
+                    <Welcome />
+                    <PopularJobs />
+                    <NearByJobs />
+                </View>
+
+            </ScrollView>
+        </SafeAreaView>
+    );
+};
+
+
+export default Home;
+
+const styles = StyleSheet.create({
+    SafeAreaView: {
+        flex: 1,
+        backgroundColor: COLORS.background,
+        justifyContent: "center",
+        // alignItems: "center"
+    },
+    HomePage: {
+        flex: 1,
+        padding: SIZES.medium
+    }
+})
